7e1fecaae4f8ceba8a6b12f09b0d1dd8c7942bc7
Ann-Vanessa Lartey <97880853+Ann-Vanessa@users.noreply.github.com>
2024-04-16 20:17:45 +0000

    second commit

--- request.h
+++ request.h
@@ -1,5 +1,6 @@
-#ifndef __REQUEST_H__
+#ifndef __REQUEST_H__ // Start of header guard
+#define REQUEST_H  // Define a unique constant to prevent repeated inclusion
 
 void *request_handle(void *fd_rec);
 
-#endif // __REQUEST_H__
+#endif // __REQUEST_H__ // End of header guard
